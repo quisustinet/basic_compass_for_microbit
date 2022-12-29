0c0a22fad719aed86bc89aa3692df88aaf09f360
Wojtek W <65029112+quisustinet@users.noreply.github.com>
2022-12-29 14:52:18 +0100

    Now works in 8 directions instead of 4 displaying the appropriate arrow.

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ basic.show_icon(IconNames.HAPPY)
 def on_forever():
     if input.compass_heading() > 358 or input.compass_heading() < 2:
         basic.show_arrow(ArrowNames.NORTH)
-    elif input.compass_heading() > 178 and input.compass_heading() < 182:
-        basic.show_arrow(ArrowNames.SOUTH)
+    elif input.compass_heading() > 43 and input.compass_heading() < 47:
+        basic.show_arrow(ArrowNames.NORTH_WEST)
     elif input.compass_heading() > 88 and input.compass_heading() < 92:
         basic.show_arrow(ArrowNames.WEST)
+    elif input.compass_heading() > 133 and input.compass_heading() < 137:
+        basic.show_arrow(ArrowNames.SOUTH_WEST)
+    elif input.compass_heading() > 178 and input.compass_heading() < 182:
+        basic.show_arrow(ArrowNames.SOUTH)
+    elif input.compass_heading() > 223 and input.compass_heading() < 227:
+        basic.show_arrow(ArrowNames.SOUTH_EAST)
     elif input.compass_heading() > 268 and input.compass_heading() < 272:
         basic.show_arrow(ArrowNames.EAST)
+    elif input.compass_heading() > 313 and input.compass_heading() < 317:
+        basic.show_arrow(ArrowNames.NORTH_EAST)
     else:
         basic.clear_screen()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
-basic.showIcon(IconNames.Happy)
-basic.forever(function on_forever() {
+basic.forever(function () {
     if (input.compassHeading() > 358 || input.compassHeading() < 2) {
         basic.showArrow(ArrowNames.North)
-    } else if (input.compassHeading() > 178 && input.compassHeading() < 182) {
-        basic.showArrow(ArrowNames.South)
+    } else if (input.compassHeading() > 43 && input.compassHeading() < 47) {
+        basic.showArrow(ArrowNames.NorthWest)
     } else if (input.compassHeading() > 88 && input.compassHeading() < 92) {
         basic.showArrow(ArrowNames.West)
+    } else if (input.compassHeading() > 133 && input.compassHeading() < 137) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (input.compassHeading() > 178 && input.compassHeading() < 182) {
+        basic.showArrow(ArrowNames.South)
+    } else if (input.compassHeading() > 223 && input.compassHeading() < 227) {
+        basic.showArrow(ArrowNames.SouthEast)
     } else if (input.compassHeading() > 268 && input.compassHeading() < 272) {
         basic.showArrow(ArrowNames.East)
+    } else if (input.compassHeading() > 313 && input.compassHeading() < 317) {
+        basic.showArrow(ArrowNames.NorthEast)
     } else {
         basic.clearScreen()
     }
-    
 })
